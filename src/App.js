d028be4f02a9382c3543d680f25989f632adc8a5
Rafael Ventura <raf4a@DESKTOP-5HNLOS1.localdomain>
2022-09-27 23:07:37 -0300

    pegando dados do user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Button, Link } from '@mui/material';
+import { Button } from '@mui/material';
 import axios from 'axios';
 
 function App() {
@@ -9,9 +9,11 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const [token, setToken] = useState("")
-  const [serachKey, setSearchKey] = useState("")
+  const [searchKey, setSearchKey] = useState("")
   const [artists, setArtists] = useState([])
-
+  const [topTracks, setTopTracks] = useState([])
+  const [topArtists, setTopArtists] = useState([])
+  const [me, setMe] = useState("")
   useEffect(() => {
     const hash = window.location.hash
     let token = window.localStorage.getItem("token")
@@ -39,12 +41,64 @@ function App() {
       },
       params: {
         q: searchKey,
-        type: "artist"
+        type: "artist",
+        limit: 5,
+        market: "BR"
       }
     })
 
     setArtists(data.artists.items)
   }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+      <div key={artist.id}>
+        {artist.images.length ? <img width={"50%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
+        {artist.name}
+      </div>
+    ))
+  }
+
+  const getTopTracks = async () => {
+    const data = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=50", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        type: "tracks",
+        time_range: 'medium_term',
+        limit: 3
+      }
+    })
+
+    setTopTracks(data.items)
+  }
+
+  const getTopArtists = async () => {
+    const { data } = await axios.get("https://api.spotify.com/v1/me/top/artists",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          time_range: 'short_term',
+          limit: 10
+        }
+      })
+
+    setTopArtists(data.items)
+  }
+
+  const getMe = async () => {
+    const { data } = await axios.get("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    })
+
+    setMe(data)
+  }
+
   return (
     <div className="App">
       <header className='App=header'>
@@ -54,7 +108,35 @@ function App() {
           :
           <Button variant="contained" color="error" onClick={logout}> Logout </Button>
         }
+        <br />
+        <br />
+        <Button variant="contained" color="primary" onClick={getTopTracks}> Most Listen Tracks </Button>
+        <br />
+        <br />
+        <Button variant="contained" color="primary" onClick={getTopArtists}>  Most Listen Artists </Button>
+        <br />
+        <br />
       </header>
+
+      <form onSubmit={searchArtists}>
+        <input type="text" onChange={e => setSearchKey(e.target.value)} />
+        <button type={"submit"}>Search</button>
+      </form>
+      <br />
+      <br />
+      {renderArtists()}
+      {/* {getMe()} */}
+      {me ?
+        <div>
+          <p>{me.display_name} </p>
+          <p>{me.email} </p>
+          <p>{me.followers.total} </p>
+          <img src={me.images[0].url} alt="" />
+        </div>
+        :
+        <p> No info</p>
+      }
+      <Button variant="contained" color="primary" onClick={getMe}>  Get Information </Button>
     </div >
   );
 }
