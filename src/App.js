ff2cc8a7f475d9597fd23c15b36d80fa9579d470
Rafael Ventura <raf4a@DESKTOP-5HNLOS1.localdomain>
2022-09-29 20:54:19 -0300

    finishing the endpoints and mxixng the tracks and artists getTop logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Button, Typography, TextField, Grid } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
-import { inputLabelClasses } from "@mui/material/InputLabel";
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      background: {
-        paper: '#fff',
-      }
-    }
-  });
   const CLIENT_ID = "68d1b43e09794d51b64ad4083e57429f"
   const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const [token, setToken] = useState("")
-  const [searchKey, setSearchKey] = useState("")
-  const [artists, setArtists] = useState([])
-  const [topTracks, setTopTracks] = useState([])
-  const [topArtists, setTopArtists] = useState([])
-  const [userInfo, setUserInfo] = useState("")
+  const [tops, setTop] = useState([])
+  const [limit, setLimit] = useState()
+  const [type, setType] = useState()
+  const [period, setPeriod] = useState()
 
   const SCOPES = [
     "user-top-read",
@@ -51,155 +39,110 @@ function App() {
     setToken("")
     window.localStorage.removeItem("token")
   }
-  const searchArtists = async (e) => {
+
+  const getTop = async (e) => {
     e.preventDefault()
-    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+    const { data } = await axios.get(`https://api.spotify.com/v1/me/top/${type}`, {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
       },
       params: {
-        q: searchKey,
-        type: "artist",
-        limit: 2,
-        market: "BR"
+        time_range: period,
+        limit: limit
       }
     })
 
-    setArtists(data.artists.items)
-  }
-
-  const renderArtists = () => {
-    return artists.map(artist => (
-      <div key={artist.id}>
-        {artist.images.length ? <img width={"50%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
-        {artist.name}
-      </div>
-    ))
-  }
-
-  const getTopTracks = async () => {
-    const { data } = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      params: {
-        type: "tracks",
-        time_range: 'short_term',
-        limit: 3
-      }
-    })
-
-    setTopTracks(data.items)
+    setTop(data.items)
     console.log(data.items)
   }
 
-  const getTopArtists = async () => {
-    const { data } = await axios.get("https://api.spotify.com/v1/me/top/artists",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        params: {
-          time_range: 'short_term',
-          limit: 10
-        }
-      })
 
-    setTopArtists(data.items)
-    console.log(data.items)
+  const setTypeButton = (e) => {
+    setType(e.target.value)
   }
-
-  const getUserInfo = async () => {
-    const { data } = await axios.get("https://api.spotify.com/v1/me", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-    })
-
-    setUserInfo(data)
+  const setPeriodButton = (e) => {
+    setPeriod(e.target.value)
+  }
+  const setLimitButton = (e) => {
+    setLimit(e.target.value)
   }
 
   return (
     <div className="App">
       <header className='App-header'>
-        <Typography variant='h1'
-          sx={{ color: "#1ed760" }}> Spotify React App </Typography>
+        <h1 > Spotify React App </h1>
 
         {!token ?
-          <Button variant="contained" color="success" href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}&response_type=${RESPONSE_TYPE}`}>  Conectar no Spotify  </Button>
+          <button> <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}&response_type=${RESPONSE_TYPE}`}>  Conectar no Spotify</a>  </button>
           :
-          <Button variant="contained" color="error" onClick={logout}> Logout </Button>
+          <button onClick={logout}> Logout </button>
         }
         <br />
-        <Button variant="contained" color="primary" onClick={getTopTracks}> Most Listen Tracks </Button>
-        <br />
-        <Button variant="contained" color="primary" onClick={getTopArtists}>  Most Listen Artists </Button>
+
+        <form onSubmit={getTop}>
+          <h3> Choose the type </h3>
+          <div onChange={setTypeButton}>
+            <input type="radio"
+              name="type"
+              value={"tracks"}
+            /> Tracks
+
+            <input
+              type="radio"
+              name="type"
+              value={"artists"}
+            /> Artists
+
+          </div>
+          <h3> Choose the time period </h3>
+          <div onChange={setPeriodButton}>
+            <input type="radio"
+              name="period"
+              id="short_term"
+              value={"short_term"} /> 1 Month
+            <input type="radio"
+              name="period"
+              id="medium_term"
+              value={"medium_term"} /> 6 Months
+            <input type="radio"
+              name="period"
+              id="long_term"
+              value={"long_term"} /> All Time
+          </div>
+          <h3>Choose the frame limit </h3>
+
+          <input type="number" name="frame_limit" id="frame_limit" min={1} max={30}
+            onChange={setLimitButton} /> (between 1 and 30)
+          <br />
+          <button type={"submit"}> Most Listen  </button>
+        </form>
         <br />
 
         <div>
-          {topArtists.map((artist) => (
-            <div key={artist.id}>
-              <Typography variant='h2'> {artist.name} </Typography>
-              <Typography variant='h4'> {artist.followers.total} Fans </Typography>
-              <ul> {artist.genres.map((genre) => <li> <Typography variant='subtitle'> {genre} </Typography>  </li>)}</ul>
-              <img width={"50%"} src={artist.images[0].url} alt="" />
-            </div>
-          ))
+
+          {type === 'artists' ?
+            tops.map((artist) => (
+              <div key={artist.id}>
+                <h2> {artist.name} </h2>
+                <h4> {artist.followers.total} Fans </h4>
+                <ul> {artist.genres.map((genre) => <li>  {genre}   </li>)}</ul>
+                <img width={"50%"} src={artist.images[0].url} alt="" />
+              </div>
+            ))
+            :
+            <ul>
+              {tops.map((track) => (
+                <div key={track.id}>
+                  <li> <p> <b> {track.name} </b> -  {track.artists[0].name} </p> </li>
+                  <li> <p> {track.album.name} </p> </li>
+                  <img width={'20%'} src={track.album.images[0].url} alt="" />
+                  <p> ______________________________________</p>
+                </div>
+              ))}
+            </ul>
           }
         </div >
-
-        <form onSubmit={searchArtists} >
-          <Grid container spacing={1}>
-            <Grid xs={2}></Grid>
-            <Grid xs={4}>
-              <TextField id="outlined-basic"
-                label="Digite um Artista"
-                variant="outlined"
-                size='small'
-                sx={{
-                  input: {
-                    color: "#1ed760",
-                    borderRadius: "6%",
-                    borderColor: "#fff"
-
-                  }
-                }}
-                InputLabelProps={{
-                  sx: {
-                    color: "#1ed760",
-                    [`&.${inputLabelClasses.shrink}`]: {
-                      // set the color of the label when shrinked (usually when the TextField is focused)
-                      color: "#1ed760"
-                    }
-                  }
-                }}
-                onChange={e => setSearchKey(e.target.value)} />
-            </Grid>
-
-            <Button type={"submit"} variant="contained" color="primary">  Get Information </Button>
-
-            <Grid xs={1}>
-              <Button> <CloseOutlinedIcon sx={{ color: "red" }} ></CloseOutlinedIcon> </Button>
-            </Grid>
-          </Grid>
-        </form>
-
-        {renderArtists()}
-
-        {
-          userInfo ?
-            <div>
-              < p > {userInfo.display_name} </p>
-              <p>{userInfo.email} </p>
-              <p>{userInfo.followers.total} followers</p>
-              <img src={userInfo.images[0].url} alt="" />
-            </div>
-            :
-            <p> No info</p>
-        }
-        <Button variant="contained" color="primary" onClick={getUserInfo}>  Get Information </Button>
-
       </header >
 
     </div >
---
+++ src/TopArtists.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function TopArtists() {
+    return (
+        <div>
+
+        </div >
+    )
+}
+export default TopArtists;
