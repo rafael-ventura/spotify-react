effea41b9009b23f956c24c59708d7c7a758f6d5
Rafael Ventura <raf4ael@gmail.com>
2025-01-24 03:01:35 -0300

    AJUSTE deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
-import { TimelineComponent } from './features/timeline/timeline.component';
-import { CallbackComponent } from './features/callback/callback.component';
+import { HomeComponent } from '@features/home/home.component';
+import { TimelineComponent } from '@features/timeline/timeline.component';
+import { CallbackComponent } from '@features/callback/callback.component';
 import { AuthGuard } from './guards/auth.guard';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,6 +19,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   spotify: {
     clientId: '68d1b43e09794d51b64ad4083e57429f',
-    redirectUri: 'https://rafael-ventura.github.io/spotify-timeline/callback',
+    redirectUri: 'https://rafael-ventura.github.io/spotify-timeline/#/callback',
     scopes: [
       'user-read-recently-played',
       'user-top-read',
